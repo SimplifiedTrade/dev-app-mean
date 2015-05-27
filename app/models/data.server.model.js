bc1c443697043e786f660d79faa99836c94696d8
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 03:01:20 -0700

    Update data.server.model.js

--- app/models/data.server.model.js
+++ app/models/data.server.model.js
@@ -5,6 +5,7 @@ var itemSchema = new mongoose.Schema({
 	created_by: { type: Schema.ObjectId, ref: 'User' },
 	created_at: {type: Date, default: Date.now},
 	UPC: String,
+	Description: String,
 	Unit_Size: String, 
 	Unit_Measure: String,
 	Case_Units: String,
@@ -12,7 +13,16 @@ var itemSchema = new mongoose.Schema({
 	Unit_Price_Delivered: String,
 	Suggested_Retail_Price: String,
 	Margin_FOB: String,
-	Margin_Delivered: String
+	Margin_Delivered: String,
+	Shelf_Life_DOM_Days: String,
+	Shelf-Life_First_REC_Days: String,
+	Internal_Reference_Code: String,
+	Internal_Item_Number: String,
+	FOB_Case_Price: String,
+	Delivered_Case_Price: String,
+	Pallet_Ti_Hi: String,
+	Case_Net_Wt_LBS: String,
+	Case_UCC: String
 });
 
 
