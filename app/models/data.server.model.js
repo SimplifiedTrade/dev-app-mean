99dfb440873f57a42102a2bdda06f77b01938df0
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-26 18:18:23 -0700

    Update data.server.model.js

--- app/models/data.server.model.js
+++ app/models/data.server.model.js
@@ -2,9 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var itemSchema = new mongoose.Schema({
-	created_by: { type: Schema.ObjectId, ref: 'User' },		//should be changed to ObjectId, ref "User"
+	created_by: { type: Schema.ObjectId, ref: 'User' },
 	created_at: {type: Date, default: Date.now},
-	text: String
+	UPC: String,
+	Unit_Size: String, 
+	Unit_Measure: String,
+	Case_Units: String,
+	Unit_Price_FOB: String,
+	Unit_Price_Delivered: String,
+	Suggested_Retail_Price: String,
+	Margin_FOB: String,
+	Margin_Delivered: String
 });
 
 
