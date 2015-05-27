61a593ba36486d69dcb88f949f7197d995b4392f
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-26 23:19:08 -0700

    Update item.client.controller.js

--- public/modules/core/controllers/item.client.controller.js
+++ public/modules/core/controllers/item.client.controller.js
@@ -1,13 +1,28 @@
 'use strict';
 
-
-angular.module('core').controller('itemController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('itemController', ['$scope', 'Authentication', 'itemService'
+	function($scope, Authentication, itemService) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-
-        // Some example string
+	
+	// Some example string - remove in prod
         $scope.helloText = 'Item Test';
-        $scope.descriptionText = 'It is an application skeleton for a typical MEAN web app. You can use it to quickly bootstrap your project.';
+
+
+    	$scope.items = itemService.query();
+		$scope.newItem = "";
+
+	$scope.post = function() {
+		itemService.save({created_by: $scope.authentication.user, UPC: $scope.newItem, created_at: Date.now()}, 
+		function(){
+			$scope.items = itemService.query();
+			$scope.newItem = "";	
+		});
+	};
+	$scope.delete = function(item)	{
+		itemService.delete({id: item._id});
+		$scope.item = itemService.query();
+	};
+
 	}
 ]);
