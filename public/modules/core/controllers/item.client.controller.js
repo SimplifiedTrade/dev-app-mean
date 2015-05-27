d3aac670e814ff0d46bcbb82459a8d14a8b19462
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 00:33:08 -0700

    Update item.client.controller.js

--- public/modules/core/controllers/item.client.controller.js
+++ public/modules/core/controllers/item.client.controller.js
@@ -11,13 +11,13 @@ angular.module('core').controller('itemController', ['$scope', 'Authentication',
 
 
     	$scope.items = itemService.query();
-		$scope.newItem = {created_by: '', UPC: '', created_at: ''};;
+		$scope.newItem = '';
 
 	$scope.post = function() {
 		itemService.save({created_by: $scope.authentication.user.displayName, UPC: $scope.newItem, created_at: Date.now()}, 
 		function(){
 			$scope.items = itemService.query();
-			$scope.newItem = {created_by: '', UPC: '', created_at: ''};;	
+			$scope.newItem = '';	
 		});
 	};
 	$scope.delete = function(item)	{
