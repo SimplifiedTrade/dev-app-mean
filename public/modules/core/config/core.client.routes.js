73d9d38cd8a415de5d41adfdc18283640f676b9e
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 20:33:30 -0700

    Update core.client.routes.js

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -23,26 +23,16 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('itemformswizard', {
 			url: '/item-form',
 			templateUrl: 'modules/core/views/form.item.client.view.html'
-			//,
-		//	resolve: {
-		///	loadPlugin: function ($ocLazyLoad) {
-		//	return $ocLazyLoad.load([
-                      //  {
-                  //          files: ['modules/core/css/plugins/steps/jquery.steps.css']
-                    //    }
-                //	    ]);
-                //	}
-            	//	}
 		}).
-        	state('iitemformswizardstepone', {
+        	state('iitemformswizard.step_one', {
         		url: '/item-form/step-one',
             		templateUrl: 'modules/core/views/wizard/step_one.html'
         	})
-        	.state('itemformswizardsteptwo', {
+        	.state('itemformswizard.step_two', {
         		url: '/item-form/step-two',
         		 templateUrl: 'modules/core/views/wizard/step_two.html'
         	})
-        	.state('itemformswizardstepthree', {
+        	.state('itemformswizard.step_three', {
             		url: '/item-form/step-three',
             		templateUrl: 'modules/core/views/wizard/step_three.html'
         	})
