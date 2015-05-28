ca33d25451e2b49c7eeffb9cb602ee431a3e6fbd
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 18:30:16 -0700

    Update core.client.directives.js

--- public/modules/core/directives/core.client.directives.js
+++ public/modules/core/directives/core.client.directives.js
@@ -37,5 +37,25 @@ angular.module('core')
                 };
             }
         };
-    });
+    }).directive("uiRangeSlider", [function () {
+        return{restrict: "A", link: function (scope, ele) {
+            return ele.slider()
+        }}
+    }]).directive("uiFileUpload", [function () {
+        return{restrict: "A", link: function (scope, ele) {
+            return ele.bootstrapFileInput()
+        }}
+    }]).directive("uiSpinner", [function () {
+        return{restrict: "A", compile: function (ele) {
+            return ele.addClass("ui-spinner"), {post: function () {
+                return ele.spinner()
+            }}
+        }}
+    }]).directive("uiWizardForm", [function () {
+        return{link: function (scope, ele) {
+            return ele.steps()
+        }}
+    }])
+    
+    ;
 
