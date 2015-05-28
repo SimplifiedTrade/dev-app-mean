6f50c0f92f91ca8061b14dc9f212da18d500f783
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 20:05:28 -0700

    Update core.client.routes.js

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -20,8 +20,8 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/item',
 			templateUrl: 'modules/core/views/item.client.view.html'
 		}).
-		state('wizard', {
-			url: '/itemform',
+		state('item.forms.wizard', {
+			url: '/item-form',
 			templateUrl: 'modules/core/views/form.item.client.view.html',
 			resolve: {
 			loadPlugin: function ($ocLazyLoad) {
