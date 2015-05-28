27e46bd033b9b8c6df897dc367c28fd3f0681880
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-27 20:08:08 -0700

    Update core.client.routes.js

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -22,7 +22,16 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		}).
 		state('item.forms.wizard', {
 			url: '/item-form',
-			templateUrl: 'modules/core/views/form.item.client.view.html'
+			templateUrl: 'modules/core/views/form.item.client.view.html',
+			resolve: {
+			loadPlugin: function ($ocLazyLoad) {
+			return $ocLazyLoad.load([
+                        {
+                            files: ['modules/core/css/plugins/steps/jquery.steps.css']
+                        }
+                	    ]);
+                	}
+            		}
 		}).
         	state('item.forms.wizard.step_one', {
         		url: '/item-form/step_one',
