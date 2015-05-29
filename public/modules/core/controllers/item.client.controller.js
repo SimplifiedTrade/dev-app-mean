42cd3ba20dd5c78013a79d92c1d0313e6fa464fa
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-29 01:45:18 -0700

    Update item.client.controller.js

--- public/modules/core/controllers/item.client.controller.js
+++ public/modules/core/controllers/item.client.controller.js
@@ -18,29 +18,30 @@ angular.module('core').controller('itemController', ['$scope', '$window' ,'Authe
 	$scope.post = function() {
 	  itemService.save({
 
-		  item.created_by: $scope.authentication.user._id
-		, item.created_by_name: $scope.authentication.user.displayName
-		, item.created_at: $scope.newItem.created_at
-		, item.UPC: $scope.newItem.UPC
-		, item.Description: $scope.newItem.Description
-		, item.Product_Group: $scope.newItem.Product_Group
-		, item.Unit_Size: $scope.newItem.Unit_Size
-		, item.Unit_Measure: $scope.newItem.Unit_Measure
-		, item.Case_Units: $scope.newItem.Case_Units
-		, item.Unit_Price_FOB: $scope.newItem.Unit_Price_FOB
-		, item.Unit_Price_Delivered: $scope.newItem.Unit_Price_Delivered
-		, item.Suggested_Retail_Price: $scope.newItem.Suggested_Retail_Price
-		, item.Margin_FOB: $scope.newItem.Margin_FOB
-		, item.Margin_Delivered: $scope.newItem.Margin_Delivered
-		, item.Shelf_Life_DOM_Days: $scope.newItem.Shelf_Life_DOM_Days
-		, item.Shelf_Life_First_REC_Days: $scope.newItem.Shelf_Life_First_REC_Days
-		, item.Internal_Reference_Code: $scope.newItem.Internal_Reference_Code
-		, item.Internal_Item_Number: $scope.newItem.Internal_Item_Number
-		, item.FOB_Case_Price: $scope.newItem.FOB_Case_Price
-		, item.Delivered_Case_Price: $scope.newItem.Delivered_Case_Price
-		, item.Pallet_Ti_Hi: $scope.newItem.Pallet_Ti_Hi
-		, item.Case_Net_Wt_LBS: $scope.newItem.Case_Net_Wt_LBS
-		, item.Case_UCC: $scope.newItem.Case_UCC
+		  created_by: $scope.authentication.user._id
+		, created_by_name: $scope.authentication.user.displayName
+		, created_at: $scope.newItem.created_at
+		, UPC: $scope.newItem.UPC
+		, Description: $scope.newItem.Description
+		, Product_Group: $scope.newItem.Product_Group
+		, Unit_Size: $scope.newItem.Unit_Size
+		, Unit_Measure: $scope.newItem.Unit_Measure
+		, Case_Units: $scope.newItem.Case_Units
+		, Unit_Price_FOB: $scope.newItem.Unit_Price_FOB
+		, Unit_Price_Delivered: $scope.newItem.Unit_Price_Delivered
+		, Suggested_Retail_Price: $scope.newItem.Suggested_Retail_Price
+		, Margin_FOB: $scope.newItem.Margin_FOB
+		, Margin_Delivered: $scope.newItem.Margin_Delivered
+		, Shelf_Life_DOM_Days: $scope.newItem.Shelf_Life_DOM_Days
+		, Shelf_Life_First_REC_Days: $scope.newItem.Shelf_Life_First_REC_Days
+		, Internal_Reference_Code: $scope.newItem.Internal_Reference_Code
+		, Internal_Item_Number: $scope.newItem.Internal_Item_Number
+		, FOB_Case_Price: $scope.newItem.FOB_Case_Price
+		, Delivered_Case_Price: $scope.newItem.Delivered_Case_Price
+		, Pallet_Ti_Hi: $scope.newItem.Pallet_Ti_Hi
+		, Case_Net_Wt_LBS: $scope.newItem.Case_Net_Wt_LBS
+		, Case_UCC: $scope.newItem.Case_UCC
+
 
 	  }, function(){
 	    $scope.items = itemService.query();
