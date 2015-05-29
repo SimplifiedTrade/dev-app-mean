e4aee09f4721563765927fb9bee117072eea3c92
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-29 01:13:58 -0700

    Update item.client.controller.js

--- public/modules/core/controllers/item.client.controller.js
+++ public/modules/core/controllers/item.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('itemController', ['$scope', 'Authentication', 'itemService',
-	function($scope, Authentication, itemService) {
+angular.module('core').controller('itemController', ['$scope', '$window' ,'Authentication', 'itemService',
+	function($scope, $window, Authentication, itemService) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 	
@@ -11,14 +11,40 @@ angular.module('core').controller('itemController', ['$scope', 'Authentication',
 
 
     	$scope.items = itemService.query();
-		$scope.newItem = '';
-		$scope.UPCnumber = '';
+		$scope.newItem = {};
+	
 
 
 	$scope.post = function() {
-	  itemService.save({created_by: $scope.authentication.user._id, UPC: $scope.UPCnumber}, function(){
+	  itemService.save({
+	  	  item.created_by = req.body.created_by
+		, item.created_by_name = req.body.created_by_name
+		, item.UPC = req.body.UPC
+		, item.Description = req.body.Description
+		, item.Product_Group = req.body.Product_Group
+		, item.Unit_Size = req.body.Unit_Size
+		, item.Unit_Measure = req.body.Unit_Measure
+		, item.Case_Units = req.body.Case_Units
+		, item.Unit_Price_FOB = req.body.Unit_Price_FOB
+		, item.Unit_Price_Delivered = req.body.Unit_Price_Delivered
+		, item.Suggested_Retail_Price = req.body.Suggested_Retail_Price
+		, item.Margin_FOB = req.body.Margin_FOB
+		, item.Margin_Delivered = req.body.Margin_Delivered
+		, item.Shelf_Life_DOM_Days = req.body.Shelf_Life_DOM_Days
+		, item.Shelf_Life_First_REC_Days = req.body.Shelf_Life_First_REC_Days
+		, item.Internal_Reference_Code = req.body.Internal_Reference_Code
+		, item.Internal_Item_Number = req.body.Internal_Item_Number
+		, item.FOB_Case_Price = req.body.FOB_Case_Price
+		, item.Delivered_Case_Price = req.body.Delivered_Case_Price
+		, item.Pallet_Ti_Hi = req.body.Pallet_Ti_Hi
+		, item.Case_Net_Wt_LBS = req.body.Case_Net_Wt_LBS
+		, item.Case_UCC = req.body.Case_UCC
+
+	  	
+	  }, function(){
 	    $scope.items = itemService.query();
-	    $scope.newItem = '';
+	    $scope.newItem = {};
+	    $window.location.href = '/#!/item-form';
 	  });
 	};
 	
