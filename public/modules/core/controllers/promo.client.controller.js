e2c6f598b02af4989164f31cefb1804350ddb691
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-28 22:57:05 -0700

    Update promo.client.controller.js

--- public/modules/core/controllers/promo.client.controller.js
+++ public/modules/core/controllers/promo.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('core').controller('promoController', ['$scope', '$window',  'Authentication', 'promoService',
-	function($scope, Authentication, promoService) {
+	function($scope, $window, Authentication, promoService) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 	
