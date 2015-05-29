7f9cb72a9da8f70080d01dff57f06261a5b43735
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-28 22:14:44 -0700

    Update promo.client.controller.js

--- public/modules/core/controllers/promo.client.controller.js
+++ public/modules/core/controllers/promo.client.controller.js
@@ -18,9 +18,24 @@ angular.module('core').controller('promoController', ['$scope', 'Authentication'
 	  promoService.save({
 	  	  created_by: $scope.authentication.user._id
 	  	, created_by_name: $scope.authentication.user.displayName
-	  	, Account: $scope.newPromo.Account
-	  	, Deal_Type: $scope.newPromo.Deal_Type
-	  	, Deal_ID: $scope.newPromo.Deal_ID
+		, Deal_Type: $scope.newPromo.Deal_Type
+		, Account: $scope.newPromo.Account
+		, Product_Group: $scope.newPromo.Product_Group
+		, Feature: $scope.newPromo.Feature
+		, Display_Level: $scope.newPromo.Display_Level
+		, Normal_Retail: $scope.newPromo.Normal_Retail
+		, Promo_Price: $scope.newPromo.Promo_Price
+		, Allowance_Type: $scope.newPromo.Allowance_Type
+		, Allowance_Amount: $scope.newPromo.Allowance_Amount
+		, Retailer_Margin: $scope.newPromo.Retailer_Margin
+		, Start_Date: $scope.newPromo.Start_Date
+		, End_Date: $scope.newPromo.End_Date
+		, Allocation: $scope.newPromo.Allocation
+		, Allocation_Quantity: $scope.newPromo.Allocation_Quantity
+		, Allocation_UOM: $scope.newPromo.Allocation_UOM
+		, Forecast_Units: $scope.newPromo.Forecast_Units
+		, ROI: $scope.newPromo.ROI
+		, Notes: $scope.newPromo.Notes
 
 	  	}, function(){
 	    $scope.promos = promoService.query();
