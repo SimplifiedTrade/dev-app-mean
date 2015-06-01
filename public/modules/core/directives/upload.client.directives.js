7959166bcc0bc51873c9b8f2cd7e6ce91eb9e897
kevinpettit <kevin.a.pettit@gmail.com>
2015-05-31 17:04:17 -0700

    Create upload.client.directives.js

---
+++ public/modules/core/directives/upload.client.directives.js
@@ -0,0 +1,41 @@
+/**
+ * dropZone - Directive for Drag and drop zone file upload plugin
+ */
+function dropZone() {
+    return function(scope, element, attrs) {
+        element.dropzone({
+            url: "/api/photo",
+            maxFilesize: 100,
+            paramName: "uploadfile",
+            maxThumbnailFilesize: 5,
+            init: function() {
+                scope.files.push({file: 'added'});
+                this.on('success', function(file, json) {
+                });
+                this.on('addedfile', function(file) {
+                    scope.$apply(function(){
+                        alert(file);
+                        scope.files.push({file: 'added'});
+                    });
+                });
+                this.on('drop', function(file) {
+                    alert('file');
+                });
+            }
+        });
+    }
+}
+
+
+
+
+
+
+/**
+ *
+ * Pass all functions into module
+ */
+angular
+    .module('core')
+
+    .directive('dropZone', dropZone)
