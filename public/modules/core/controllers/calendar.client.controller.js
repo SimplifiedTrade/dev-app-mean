e50d445e3f507c18340c5e64547a0350bcde1f38
kevinpettit <kevin.a.pettit@gmail.com>
2015-06-14 19:54:17 -0700

    Create calendar.client.controller.js

---
+++ public/modules/core/controllers/calendar.client.controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+google.load('visualization', '1', {
+  packages: ['corechart']
+});
+ 
+google.setOnLoadCallback(function() {
+  angular.bootstrap(document.body, ['core']);
+});
+
+angular.module('core').controller('calendarController', ['$scope', 'Authentication',
+	function($scope, Authentication) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+
+        // Some example string
+        $scope.helloText = 'Test';
+
+    function($scope) {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ]);
+      var options = {
+        title: 'Company Performance'
+      };
+      var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
+ 
+      chart.draw(data, options);
+    }
+	
+}
+
+]);
