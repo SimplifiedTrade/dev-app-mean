6a0b0ebd58b5c8991167ef0cee81bb802b6acbe2
kevinpettit <kevin.a.pettit@gmail.com>
2015-06-14 22:03:38 -0700

    Update calendar.client.controller.js

--- public/modules/core/controllers/calendar.client.controller.js
+++ public/modules/core/controllers/calendar.client.controller.js
@@ -1,12 +1,5 @@
 'use strict';
 
-google.load('visualization', '1', {
-  packages: ['corechart']
-});
- 
-google.setOnLoadCallback(function() {
-  angular.bootstrap(document.body, ['core']);
-});
 
 angular.module('core').controller('calendarController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
