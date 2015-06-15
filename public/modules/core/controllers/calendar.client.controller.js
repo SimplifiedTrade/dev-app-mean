91b9838d8906e1e58cfafd0311ab6793992c1ed6
kevinpettit <kevin.a.pettit@gmail.com>
2015-06-14 22:13:51 -0700

    Update calendar.client.controller.js

--- public/modules/core/controllers/calendar.client.controller.js
+++ public/modules/core/controllers/calendar.client.controller.js
@@ -1,17 +1,36 @@
-'use strict';
+(function() {'use strict';
 
+google.load('visualization', '1', {
+  packages: ['corechart']
+});
+ 
+google.setOnLoadCallback(function() {
+  angular.bootstrap(document.body, ['core']);
+});
 
 angular.module('core').controller('calendarController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
+  function($scope, Authentication) {
+    // This provides Authentication context.
+    $scope.authentication = Authentication;
 
         // Some example string
         $scope.helloText = 'Test';
 
-
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ]);
+      var options = {
+        title: 'Company Performance'
+      };
+      var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
+ 
+      chart.draw(data, options);
     
-	
+  
 }
 
-]);
+]); });
