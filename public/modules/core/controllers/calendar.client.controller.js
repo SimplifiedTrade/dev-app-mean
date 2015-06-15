05cab6bf1c9599b913eaef5221d54b93c06afdec
kevinpettit <kevin.a.pettit@gmail.com>
2015-06-14 22:01:41 -0700

    Update calendar.client.controller.js

--- public/modules/core/controllers/calendar.client.controller.js
+++ public/modules/core/controllers/calendar.client.controller.js
@@ -16,19 +16,7 @@ angular.module('core').controller('calendarController', ['$scope', 'Authenticati
         // Some example string
         $scope.helloText = 'Test';
 
-      var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses'],
-        ['2004', 1000, 400],
-        ['2005', 1170, 460],
-        ['2006', 660, 1120],
-        ['2007', 1030, 540]
-      ]);
-      var options = {
-        title: 'Company Performance'
-      };
-      var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
- 
-      chart.draw(data, options);
+
     
 	
 }
