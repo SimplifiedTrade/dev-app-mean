1a865ac8e3660a3c29b33246fad32056d3ed3be3
kevinpettit <kevin.a.pettit@gmail.com>
2015-06-21 19:03:17 +0000

    update

--- public/modules/core/controllers/calendar.client.controller.js
+++ public/modules/core/controllers/calendar.client.controller.js
@@ -1,43 +1,34 @@
  'use strict';
-
     angular.module('core').controller('calendarController', function($scope) {
       $scope.name = "blah"
     });
-
    angular.module('core').directive('chart', function() {
         return {
           restrict: 'A',
           link: function($scope, $elm, $attr) {
             // Create the data table.
             var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-              ['Mushrooms', 3],
-              ['Onions', 1],
-              ['Olives', 1],
-              ['Zucchini', 1],
-              ['Pepperoni', 2]
-            ]);
+             data.addColumn({ type: 'string', id: 'President' });
+             data.addColumn({ type: 'date', id: 'Start' });
+             data.addColumn({ type: 'date', id: 'End' });
+             data.addRows([
+            [ 'Washington', new Date(1789, 3, 29), new Date(1797, 2, 3) ],
+            [ 'Adams',      new Date(1797, 2, 3),  new Date(1801, 2, 3) ],
+            [ 'Jefferson',  new Date(1801, 2, 3),  new Date(1809, 2, 3) ]]);
 
+            
             // Set chart options
-            var options = {'title':'How Much Pizza I Ate Last Night',
-                           'width':400,
-                           'height':300};
-
+            var options = {'title':'Timeline',
+                           'width':800,
+                           'height':800};
             // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart($elm[0]);
+            var chart = new google.visualization.Timeline($elm[0]);
             chart.draw(data, options);
           }
       }
     });
-
-
 //    google.setOnLoadCallback(function() {
-//      angular.bootstrap(document.body, ['core']);
+//      angular.bootstrap(document.body, ['myapp']);
 //    });
-    google.load('visualization', '1', {packages: ['corechart']});
-
-
-
+    google.load('visualization', '1', {packages: ['timeline']});
 
